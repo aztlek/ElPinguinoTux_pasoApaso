874f1f4225ad2e0e0ff89e0303b592cb1cc3825e
aztlek <aztlek@durnak.aztlek.org>
2016-02-15 10:34:05 -0500

    Dibujar las otras partes y volver a la translación y escalación anterior

--- ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Tux.java
+++ ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Tux.java
@@ -18,6 +18,7 @@ package elpinguinotux_pasoapaso;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.Polygon;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 
@@ -76,13 +77,54 @@ public class Tux {
         graphics2D.translate(getX(), getY());
         graphics2D.scale(getEscalaX(), getEscalaY());
         
-                // Cuerpo
+        // Cuerpo
         graphics2D.setPaint(Color.BLACK);
         graphics2D.fill(new Ellipse2D.Double(7d, 0d, 176d, 230d));
 
         // Pecho
         graphics2D.setPaint(Color.WHITE);
         graphics2D.fill(new Ellipse2D.Double(26d, 83d, 138d, 145d));
+
+        // Pico
+        graphics2D.setPaint(new Color(255, 149, 0));
+        int picox[] = {80, 110, 95};
+        int picoy[] = {65, 65, 90};
+        graphics2D.fill(new Polygon(picox, picoy, picox.length));
+
+        // Aleta derecha
+        graphics2D.setPaint(Color.BLACK);
+        graphics2D.fill(new Ellipse2D.Double(0d, 80d, 44d, 127d));
+
+        // Aleta izquierda
+        graphics2D.setPaint(Color.BLACK);
+        graphics2D.fill(new Ellipse2D.Double(146d, 80d, 44d, 127d));
+
+        // Ojo derecho
+        graphics2D.setPaint(Color.WHITE);
+        graphics2D.fill(new Ellipse2D.Double(49d, 13d, 41d, 51d));
+
+        // Pupila derecha
+        graphics2D.setPaint(Color.BLACK);
+        graphics2D.fill(new Ellipse2D.Double(71d, 44d, 13d, 15d));
+
+        // Ojo izquierdo
+        graphics2D.setPaint(Color.WHITE);
+        graphics2D.fill(new Ellipse2D.Double(100d, 13d, 41d, 51d));
+
+        // Pupila izquierda
+        graphics2D.setPaint(Color.BLACK);
+        graphics2D.fill(new Ellipse2D.Double(107d, 44d, 13d, 15d));
+
+        // Pata derecha
+        graphics2D.setPaint(new Color(255, 149, 0));
+        graphics2D.fill(new Ellipse2D.Double(27d, 209d, 50d, 28d));
+
+        // Pata  izquierda
+        graphics2D.setPaint(new Color(255, 149, 0));
+        graphics2D.fill(new Ellipse2D.Double(113d, 209d, 50d, 28d));
+
+        // Volver a la translación y escalación anterior
+        graphics2D.setTransform(affineTransform);
 
     }
     
