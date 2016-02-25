5a0347a59b8145e78c7ae0dfdd15881028dd818d
aztlek <aztlek@durnak.aztlek.org>
2016-02-25 15:18:22 -0500

    Creación de la clase Orca

--- ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Orca.java
+++ ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Orca.java
@@ -16,79 +16,10 @@
  */
 package elpinguinotux_pasoapaso;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Polygon;
-import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
-
 /**
  *
  * @author aztlek
  */
 public class Orca {
-
-    private final double totalWidth = 190.0d;
-    private final double totalHeight = 237.0d;
-    private final double x, y;
-    private final double width, height;
-    private final double escalaX, escalaY;
-
-    public Orca(double x, double y, double width, double height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.escalaX = width / totalWidth;
-        this.escalaY = height / totalHeight;
-    } // Orcar()
-
-    public void paint(Graphics2D graphics2D) {
-        // Transladar y escalar
-        AffineTransform affineTransform = graphics2D.getTransform();
-        graphics2D.translate(getX(), getY());
-        graphics2D.scale(getEscalaX(), getEscalaY());
-
-        // Poner aquí los dibujos del objeto gráfico
-        
-        // Rejilla de referencia
-        new Grid(getTotalWidth(), getTotalHeight()).paint(graphics2D);
-
-        // Volver a la translación y escalación anterior
-        graphics2D.setTransform(affineTransform);
-
-    } // paint()
-
-    public double getTotalWidth() {
-        return totalWidth;
-    }
-
-    public double getTotalHeight() {
-        return totalHeight;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
-    public double getEscalaX() {
-        return escalaX;
-    }
-
-    public double getEscalaY() {
-        return escalaY;
-    }
-
-} // class Orca
+    
+}
