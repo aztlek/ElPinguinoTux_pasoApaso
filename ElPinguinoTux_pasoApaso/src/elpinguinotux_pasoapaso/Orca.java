70927afb5086873d376698d0c7847eb77d736b25
aztlek <aztlek@durnak.aztlek.org>
2016-02-25 15:20:34 -0500

    Declarando la clase Orca

--- ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Orca.java
+++ ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Orca.java
@@ -20,3 +20,110 @@ package elpinguinotux_pasoapaso;
  *
  * @author aztlek
  */
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+
+/**
+ *
+ * @author aztlek
+ */
+public class Orca {
+
+    private final double totalWidth = 280.0d;
+    private final double totalHeight = 160.0d;
+    private final double x, y;
+    private final double width, height;
+    private final double escalaX, escalaY;
+
+    public Orca(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.escalaX = width / totalWidth;
+        this.escalaY = height / totalHeight;
+    } // Orca()
+
+    public void paint(Graphics2D graphics2D) {
+        // Transladar y escalar
+        AffineTransform affineTransform = graphics2D.getTransform();
+        graphics2D.translate(getX(), getY());
+        graphics2D.scale(getEscalaX(), getEscalaY());
+
+        // Cuerpo
+        graphics2D.setColor(Color.BLACK);
+        graphics2D.fill(new Ellipse2D.Double(50, 65, 230, 71));
+
+        // Aleta caudal
+        graphics2D.setColor(Color.BLACK);
+
+        // Puntos aleta caudal
+        int[] aletaCaudalx = {0, 79, 22};
+        int[] aletaCaudaly = {63, 102, 111};
+        graphics2D.fillPolygon(aletaCaudalx, aletaCaudaly, aletaCaudalx.length);
+
+        // Puntos aleta dorsal
+        int[] aletaDorsalx = {158, 186, 150};
+        int[] aletaDorsaly = {0, 66, 66};
+        graphics2D.fillPolygon(aletaDorsalx, aletaDorsaly, aletaDorsalx.length);
+
+        // Mancha pos ocular
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fill(new Ellipse2D.Double(201, 76, 42, 20));
+
+        // Ojo
+        graphics2D.setColor(Color.RED);
+        graphics2D.fill(new Ellipse2D.Double(245, 92, 3, 3));
+
+        // Aleta pectoral
+        graphics2D.setColor(Color.BLACK);
+        graphics2D.fill(new Ellipse2D.Double(177, 122, 26, 40));
+
+        // Mancha ventral
+        graphics2D.setColor(Color.WHITE);
+        graphics2D.fill(new Ellipse2D.Double(74, 104, 58, 27));
+        graphics2D.fill(new Ellipse2D.Double(172, 102, 88, 29));
+        
+//        // Rejilla de referencia
+//        new Grid(getTotalWidth(), getTotalHeight()).paint(graphics2D);
+
+        // Volver a la translación y escalación anterior
+        graphics2D.setTransform(affineTransform);
+
+    } // paint()
+
+    public double getTotalWidth() {
+        return totalWidth;
+    }
+
+    public double getTotalHeight() {
+        return totalHeight;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getEscalaX() {
+        return escalaX;
+    }
+
+    public double getEscalaY() {
+        return escalaY;
+    }
+
+} // class Orca
