f22077f7170a41e36865de32004c4c7c8c458843
aztlek <aztlek@durnak.aztlek.org>
2016-03-10 13:45:03 -0500

    Se declaró como atributo en Escenario la referencia al vector de vectores de vectores de las hileras

--- ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Escenario.java
+++ ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Escenario.java
@@ -43,6 +43,7 @@ public class Escenario extends Canvas {
     private final Iceberg[][] icebergs;
     private final Pez[][] peces;
     private final CuboDeHielo[][] columnas;
+    private final CuboDeHielo[][][] hileras;
     
     public Escenario(int x, int y, int width, int height, JFrame marco) {
         setBounds(x, y, width, height);
