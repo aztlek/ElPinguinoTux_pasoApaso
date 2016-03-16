0784b068c4159a7e2fac7b72ef94afc3f61c2ba1
aztlek <aztlek@durnak.aztlek.org>
2016-03-16 18:31:33 -0500

    Se borraon los thows

--- ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Tux.java
+++ ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Tux.java
@@ -174,17 +174,14 @@ public class Tux implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
 } // class Tux
