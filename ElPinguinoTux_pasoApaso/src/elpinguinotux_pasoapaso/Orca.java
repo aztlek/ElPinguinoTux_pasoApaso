df1f084152d67ca5c37e2df3ef77b5be6dca3304
aztlek <aztlek@durnak.aztlek.org>
2016-03-20 17:07:37 -0500

    Se declarararon los métodos set y get para x e y  en el Villano

--- ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Orca.java
+++ ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/Orca.java
@@ -107,10 +107,18 @@ public class Orca {
         return x;
     }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
     public double getY() {
         return y;
     }
-
+    
+    public void setY(double y) {
+        this.y = y;
+    }    
+    
     public double getWidth() {
         return width;
     }
