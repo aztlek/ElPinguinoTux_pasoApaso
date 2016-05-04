32f4e9f103cff850f40a5bdf3fd0844b24a4abe8
aztlek <aztlek@durnak.aztlek.org>
2016-05-04 11:38:16 -0500

    Se puso a null el parámetro que corresponde con la meta

--- ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/ContadorVidas.java
+++ ElPinguinoTux_pasoApaso/src/elpinguinotux_pasoapaso/ContadorVidas.java
@@ -91,6 +91,7 @@ public class ContadorVidas extends ObjetoGrafico{
                     anchoPinguino,
                     altoPinguino,
                     null,
+                    null,
                     null
             ).paint(graphics2D);
         }
